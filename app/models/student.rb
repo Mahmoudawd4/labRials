0a6bf371598e56c7688f71775328144b195f3d0b
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-05-20 15:33:38 +0300

    edit asdd

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
   belongs_to :track
+
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { greater_than: 0 }
+  validates :IDno, presence: true, uniqueness: true
+  validates :track, presence: true
+  
 end

61ef25313211fe0828d831a829da1393675eeb45
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-05-20 14:36:27 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :students
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
+  # root to: "home#index"
+  root to: 'home#index'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/migrate/20230519113609_rename_people_table_to_students.rb
@@ -0,0 +1,5 @@
+class RenamePeopleTableToStudents < ActiveRecord::Migration[7.0]
+  def change
+    rename_table :people, :students
+  end
+end
